84acb89f613a7fc9c329862e8151bf61ccc01d17
MD TOFAZZOL HOSSAIN <tofazzol339036@student.sust.edu>
2021-05-11 11:56:04 +0600

    Recap core concepts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Users></Users>
       </header>
     </div>
   );
+}
+function Users(){
+  const [users, setUsers] = useState ([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  }, [])
+  console.log(users);
+  return(
+    <div>
+      <h2>Dynamic Users:{users.length}</h2>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li> )
+        }
+      </ul>
+    </div>
+  )
 }
 
 export default App;
